7a38a1df35cd24a43ea15200a00f492e47c7b228
LAKSHAY PUNIA <148634500+Lakshay-punia@users.noreply.github.com>
2024-11-22 20:35:38 +0530

    chat header component created

--- app/(main)/(routes)/server/[serverid]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/server/[serverid]/channels/[channelId]/page.tsx
@@ -1,10 +1,50 @@
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 
-const ChannelIdPage = async () => {
+interface channelIdPageProps{
+    params : {
+        serverid : string;
+        channelId : string;
+    }
+}
+
+const ChannelIdPage = async({
+    params
+} :  channelIdPageProps) => {
+
+    const profile = await currentProfile()
+
+    if(!profile){
+        return RedirectToSignIn
+    }
+
+    const channel = await db.channel.findUnique({
+        where : {
+            id : params.channelId
+        }
+    })
+
+    const member =  await db.member.findFirst({
+        where : {
+            serverId : params.serverid,
+            profileId : profile.id,
+        }
+    })
+
+    if(!channel || !member){
+        redirect("/");
+    }
 
     return (
-        <div>
-            channel id page
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader 
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel" />
         </div>
     )
 }
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -89,7 +89,7 @@ export const ServerSidebar = async ({
 
 
     return(
-        <div className="flex flex-col h-full text-primary w-fulll dark:bg-[#2B2D31] bg-[#F2F3F5] ">
+        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5] ">
             <ServerHeader
             server={server}
             role={role}
